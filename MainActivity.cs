ee7f03d5d316220f5ee065b62bc77503e991542f
Merciful Afolabi <lexmerciful@gmail.com>
2023-01-25 13:43:21 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Calligraphy;
+
+namespace Money_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            CalligraphyConfig.InitDefault(new CalligraphyConfig.Builder()
+                .SetDefaultFontPath("fonts/GlacialIndifference-Regular.ttf")
+                .SetFontAttrId(Resource.Attribute.fontPath)
+                .Build());
+
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
+        }
+
+    }
+}
